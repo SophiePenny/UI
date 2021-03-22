54f611bf85c431b19532b03ccfcffb2363319bef
SophiePenny <72386509+SophiePenny@users.noreply.github.com>
2021-03-22 10:53:33 +0000

    assets?

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Test : MonoBehaviour
+{
+    public void TestButtonScript(string myMessage)
+    {
+        Debug.Log(myMessage);
+    }
+
+    public void LoadScene(string NewGame)
+    {
+        SceneManager.LoadScene(NewGame);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
