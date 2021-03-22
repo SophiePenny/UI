2b29021bca05e4ae72b068e8a6a409599b610802
SophiePenny <72386509+SophiePenny@users.noreply.github.com>
2021-03-22 15:15:29 +0000

    static variable

---
+++ Assets/Scripts/SharedData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SharedData
+{
+    static int score = 1000;
+    static float musicVolume;
+}
