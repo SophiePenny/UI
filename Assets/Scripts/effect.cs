d2f9ea250b217eb120ebcd953fe53b5bb8d522f0
SophiePenny <72386509+SophiePenny@users.noreply.github.com>
2021-03-22 12:19:43 +0000

    sounds

---
+++ Assets/Scripts/effect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class effect : MonoBehaviour
+{
+    public AudioClip myAudioClip;
+    public AudioSource AudioSource;
+
+    public void PlaySound()
+    {
+        AudioSource.PlayOneShot(myAudioClip, 0.9f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
